2b6bfe9744dd80b37d9087ff3040062dfab25818
David Van Horn <David.VanHorn@cchmc.org>
2018-11-26 23:14:40 -0500

    added a sidebar for extra information

--- ui/src/Application.js
+++ ui/src/Application.js
@@ -1,19 +1,40 @@
 import React from "react";
-import { Layout, Panel, NavDrawer } from "react-toolbox/lib/layout";
+import { Layout, Panel, NavDrawer, Sidebar } from "react-toolbox/lib/layout";
+import { IconButton } from "react-toolbox/lib/button";
 import { AppBar } from "react-toolbox/lib/app_bar";
 
 class Application extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            drawerActive: false
+            drawerActive: false,
+            sidebarPinned: true
         };
         this.toggleDrawerActive = this.toggleDrawerActive.bind(this);
+        this.toggleSidebarPinned = this.toggleSidebarPinned.bind(this);
     }
 
     toggleDrawerActive() {
         this.setState(state => ({
             drawerActive: !this.state.drawerActive
+        }));
+    }
+
+    openSidebar() {
+        this.setState(state => ({
+            sidebarPinned: true
+        }));
+    }
+
+    closeSidebar() {
+        this.setState(state => ({
+            sidebarPinned: false
+        }));
+    }
+
+    toggleSidebarPinned() {
+        this.setState(state => ({
+            sidebarPinned: !this.state.sidebarPinned
         }));
     }
 
@@ -34,6 +55,12 @@ class Application extends React.Component {
                         <p>Main content goes here.</p>
                     </div>
                 </Panel>
+                <Sidebar pinned={ this.state.sidebarPinned } width={ 5 }>
+                    <div><IconButton icon='close' onClick={ this.toggleSidebarPinned }/></div>
+                    <div style={{ flex: 1 }}>
+                        <p>Supplemental content goes here.</p>
+                    </div>
+                </Sidebar>
             </Layout>
         );
     }
