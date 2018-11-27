cac731aae02134bc6e6b42295c4605280cccbcee
David Van Horn <David.VanHorn@cchmc.org>
2018-11-26 23:02:11 -0500

    added nav drawer

--- ui/src/Application.js
+++ ui/src/Application.js
@@ -1,14 +1,35 @@
 import React from "react";
-import { Layout, Panel } from "react-toolbox/lib/layout";
+import { Layout, Panel, NavDrawer } from "react-toolbox/lib/layout";
 import { AppBar } from "react-toolbox/lib/app_bar";
 
 class Application extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            drawerActive: false
+        };
+        this.toggleDrawerActive = this.toggleDrawerActive.bind(this);
+    }
+
+    toggleDrawerActive() {
+        this.setState(state => ({
+            drawerActive: !this.state.drawerActive
+        }));
+    }
+
     render() {
         return (
             <Layout>
+                <NavDrawer active={this.state.drawerActive}
+                    permanentAt='xxl'
+                    onOverlayClick={ this.toggleDrawerActive }>
+                    <p>
+                        Navigation, account switcher, etc. go here.
+                    </p>
+                </NavDrawer>
                 <Panel>
-                    <AppBar leftIcon='menu' />
-                    <div>
+                    <AppBar leftIcon='menu' onLeftIconClick={this.toggleDrawerActive} />
+                    <div style={{ flex: 1, overflowY: 'auto', padding: '1.8rem' }}>
                         <h1>Main Content</h1>
                         <p>Main content goes here.</p>
                     </div>
