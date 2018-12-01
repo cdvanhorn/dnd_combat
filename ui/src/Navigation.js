4338cc2d9c6e34428a1e6e3be70ffb1f3e56c3e3
cdvanhorn <cdvanhorn@hotmail.com>
2018-11-30 23:03:01 -0500

    added other menu options

--- ui/src/Navigation.js
+++ ui/src/Navigation.js
@@ -11,10 +11,30 @@ class Navigation extends React.Component {
             <List selectable ripple>
                 <ListSubHeader caption='ComBAt Menu' />
                 <ListItem
+                    leftIcon="security"
+                    caption="Combat Tracker"
+                    onClick={(e) => this.props.app.setActivePage("Combat", "Combat Tracker", e)}
+                />
+                <ListItem
+                    leftIcon="group"
+                    caption="Encounters"
+                    onClick={(e) => this.props.app.setActivePage("Encounters", "Encounters", e)}
+                />
+                <ListItem
+                    leftIcon="memory"
+                    caption="Non-Player Characters"
+                    onClick={(e) => this.props.app.setActivePage("Npcs", "Non-Player Characters", e)}
+                />
+                <ListItem
                     leftIcon="face"
                     caption="Player Characters"
-                    legend="Edit Player Characters"
                     onClick={(e) => this.props.app.setActivePage("Characters", "Player Characters", e)}
+                />
+                <ListItem
+                    leftIcon="settings"
+                    caption="Settings"
+                    legend="Settings, Bitch!"
+                    onClick={(e) => this.props.app.setActivePage("Settings", "Settings", e)}
                 />
             </List>
         );
