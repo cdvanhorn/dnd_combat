d9099d43ae5ab3ef9ea13d4b968ebde51cf3caf1
David Van Horn <David.VanHorn@cchmc.org>
2018-12-05 23:02:15 -0500

    playing with form and dropdown

--- ui/src/details/PlayerCharacter.js
+++ ui/src/details/PlayerCharacter.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 
 import {Avatar} from "react-toolbox/lib/avatar";
 import {Input} from "react-toolbox/lib/input";
+import {Dropdown} from "react-toolbox/lib/dropdown";
 
 import { CREATE_PLAYER_CHARACTER_ID } from "../redux/actions/pcs.js";
 
@@ -10,16 +11,36 @@ const mapStateToProps = state => {
     return { "selected_pc_id": state.pcs.ui_selected_pc_id };
 };
 
+function CharacterForm(props) {
+    return (
+        <div>
+            <Avatar title={props.character.name} />
+            <Input type='text' label='Character Name' name='name' value={props.character.name}/>
+            <Dropdown
+                label="Race"
+                auto
+                source={props.races}
+                value={props.character.race_id}
+            />
+            <Dropdown
+                label="Class"
+                auto
+                source={props.classes}
+                value={props.character.class_id}
+            />
+        </div>
+    )
+}
+
 class PlayerCharacterDetails extends React.Component {
     render() {
         let content = <p>Select a Player Character</p>;
         if(this.props.selected_pc_id == CREATE_PLAYER_CHARACTER_ID) {
-            content = (
-                <div>
-                <Avatar title="new" />
-                <Input type='text' label='Character Name' name='name' disabled={true}/>
-                </div>
-            );
+            content = CharacterForm({
+                character: {id: 1, name: "Steve Stephen Stevenson", race_id: 1, class_id: 1},
+                races: [{value: 1, label: "Human"}],
+                classes: [{value: 1, label: "Fighter"}]
+            });
         } else {
             //check for valid pc id, display details
             console.log("hola");
