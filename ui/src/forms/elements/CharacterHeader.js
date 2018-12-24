8df9ae5ca8a817b016c9fae87dd2fbcb4708b949
David Van Horn <David.VanHorn@cchmc.org>
2018-12-23 23:18:34 -0500

    class and race select working

--- ui/src/forms/elements/CharacterHeader.js
+++ ui/src/forms/elements/CharacterHeader.js
@@ -9,13 +9,14 @@ import {rdmCap} from "../../Utilities.js";
 
 export default class CharacterHeader extends React.Component {
     render() {
+        let raceOptions = "banana";
         return (
             <Container>
                 <Row>
                     <Col sm={1}>
                     </Col>
                     <Col sm={11}>
-                        <Form.Group as={Col} controlId="formGridSearch">
+                        <Form.Group as={Col} controlId="formGridCharacterName">
                             <Form.Label>Character Name</Form.Label>
                             <Form.Control
                                 type="text"
@@ -26,41 +27,44 @@ export default class CharacterHeader extends React.Component {
                         </Form.Group>
                     </Col>
                 </Row>
-                {/*
                 <Row>
                     <Col sm={4}>
-                        <Input
-                            type='number'
-                            label='Level'
-                            name='level'
-                            value={this.props.character.level}
-                            onChange={this.props.handleChange.bind(this, 'level')}
-                        />
+                        <Form.Group as={Col} controlId="formGridCharacterLevel">
+                            <Form.Label>Level</Form.Label>
+                            <Form.Control
+                                type="number"
+                                value={this.props.character.level}
+                                onChange={this.props.handleChange.bind(this, 'level')}
+                            />
+                        </Form.Group>
                     </Col>
                     <Col sm={4}>
-                        <Dropdown
-                            label="Race"
-                            auto
-                            source={this.props.races}
-                            value={this.props.character.race_id}
-                            labelKey={"name"}
-                            valueKey={"id"}
-                            onChange={this.props.handleChange.bind(this, 'race_id')}
-                        />
+                        <Form.Group controlId="formGridRace">
+                            <Form.Label>Race</Form.Label>
+                            <Form.Control
+                                as="select"
+                                value={this.props.character.race_id}
+                                onChange={this.props.handleChange.bind(this, 'race_id')}
+                            >
+                                <option>1</option>
+                                <option>2</option>
+                            </Form.Control>
+                        </Form.Group>
                     </Col>
                     <Col sm={4}>
-                        <Dropdown
-                            label="Class"
-                            auto
-                            source={this.props.classes}
-                            value={this.props.character.class_id}
-                            labelKey={"name"}
-                            valueKey={"id"}
-                            onChange={this.props.handleChange.bind(this, 'class_id')}
-                        />
+                        <Form.Group controlId="formGridClass">
+                            <Form.Label>Class</Form.Label>
+                            <Form.Control
+                                as="select"
+                                value={this.props.character.class_id}
+                                onChange={this.props.handleChange.bind(this, 'class_id')}
+                            >
+                                <option>1</option>
+                                <option>2</option>
+                            </Form.Control>
+                        </Form.Group>
                     </Col>
                 </Row>
-                */}
             </Container>
         );
     }
